1543ab2fbfba6b0f600fe190414063fc3b984608
Carlos Henrique <carlos.h.computacao@gmail.com>
2022-05-01 23:53:34 -0300

    array names Henrique

---
+++ ArrayNames_Henrique.js
@@ -0,0 +1,14 @@
+var array = [];
+
+function nomes(a) {
+    if (a == 0) {
+        return "Henrique";
+    } else if (a == 1) {
+        return "Jholdy";
+    } else if (a == 2) {
+        return "Adson";
+    } else if (a == 3) {
+        return "Gustavo";
+    }
+    
+};
